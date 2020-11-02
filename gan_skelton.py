65fada607fd9021f5dd375e728f1d79bf800bcec
vinrich <colin@vinarcik.com>
2020-11-02 13:39:32 -0500

    final

--- gan_skelton.py
+++ gan_skelton.py
@@ -27,8 +27,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # NOTE: mnist_d is no credit
 # NOTE: cifar_10 is extra credit
 #DATASET = "mnist_d"
-#DATASET = "mnist_f"
-DATASET = "cifar_10"
+DATASET = "mnist_f"
+#DATASET = "cifar_10"
 
 if DATASET == "mnist_d":
     IMAGE_SHAPE = (IH, IW, IZ) = (28, 28, 1)
